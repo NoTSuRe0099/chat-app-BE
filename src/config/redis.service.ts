dd0fbdfebeb9aa136f48556763b9b87577df7292
Rushikesh Patil <rp73006@gmail.com>
2024-09-14 22:51:36 +0530

    redis server connection refactor

--- src/config/redis.service.ts
+++ src/config/redis.service.ts
@@ -1,31 +1,29 @@
 import * as redis from 'redis';
 let redisClient: any;
 
-// const REDIS_URL = process.env.REDIS_CLIENT_URL;
-(async () => {
-  redisClient =
-    redis?.createClient &&
-    redis?.createClient({
-      url: 'rediss://red-cfr9d85a499b40d89pkg:g27tvaFQTDr20qr195HgtCrql65PIzht@singapore-redis.render.com:6379',
+const connectRedis = async (redisUrl: string) => {
+  if (redisUrl) {
+    redisClient =
+      redis?.createClient &&
+      redis?.createClient({
+        url: redisUrl,
+      });
+
+    redisClient.on('connect', () => {
+      console.log('Connected to Redis server');
     });
 
-  redisClient.on('connect', () => {
-    console.log('Connected to Redis server');
-  });
+    redisClient.on('error', (err: any) =>
+      console.log('Redis Client Error', err)
+    );
 
-  redisClient.on('error', (err: any) => console.log('Redis Client Error', err));
+    redisClient.on('end', () => {
+      console.log('Disconnected from Redis server');
+    });
 
-  redisClient.on('end', () => {
-    console.log('Disconnected from Redis server');
-  });
-
-  await redisClient.connect();
-
-  // Send and retrieve some valuese
-  // await client.set('key', 'node redis');
-  // const value = await client.get('key');
-
-  // console.log("found value: ", value)
-})();
+    await redisClient.connect();
+  }
+};
 
 export { redisClient };
+export default connectRedis;
